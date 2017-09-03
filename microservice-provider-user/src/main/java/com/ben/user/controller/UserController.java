aa1f272514660adddedd4d602323ec91cf4810f2
zhangchuanben <798944887@qq.com>
2017-09-03 11:31:12 +0800

    客户端调用

--- microservice-provider-user/src/main/java/com/ben/user/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/ben/user/controller/UserController.java
@@ -2,7 +2,11 @@ package com.ben.user.controller;
 
 import com.ben.user.entity.User;
 import com.ben.user.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EurekaClient eurekaClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable Long id) {
         return userRepository.findOne(id);
+    }
+
+    /**
+     * 获取ip
+     * @return
+     */
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+
+    /**
+     * 获取实例信息
+     * @return
+     */
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo() {
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
     }
 }
