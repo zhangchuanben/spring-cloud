6c98e26febd89ecf93c535375c8e1869a8ed86c7
zhangchuanben <798944887@qq.com>
2017-09-03 11:49:53 +0800

    重构了代码并为movie添加了服务发现

--- microservice-consumer-movie/src/main/java/com/ben/movie/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/ben/movie/MicroserviceConsumerMovieApplication.java
@@ -2,19 +2,19 @@ package com.ben.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import javax.xml.ws.WebEndpoint;
-
 @SpringBootApplication
-public class MicroserviceSimpleConsumerWebApplication {
+@EnableDiscoveryClient
+public class MicroserviceConsumerMovieApplication {
 
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleConsumerWebApplication.class, args);
+		SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
 	}
 }
